12e522628b1a8e0a7dc75c0c2ed21526374c7dd0
SapirLulvi <lulvi@post.bgu.ac.il>
2021-12-10 13:59:10 +0200

    Ex7

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def to_home_page():  # put application's code here
+    return redirect('/home')
 
+@app.route('/home')
+def print_welcome():
+    return 'Welcome!'
+
+
+@app.route('/about')
+def use_home_def():
+    return redirect(url_for('print_welcome'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
+
